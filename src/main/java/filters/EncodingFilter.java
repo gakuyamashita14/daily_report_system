3eaf5beed11821e785691b034ceddd0e5893d141
gaku.yamashita <gax0414@gmail.com>
2022-05-19 16:58:15 +0900

    完了

--- src/main/java/filters/EncodingFilter.java
+++ src/main/java/filters/EncodingFilter.java
@@ -1,6 +1,7 @@
 package filters;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -32,7 +33,8 @@ public class EncodingFilter implements Filter {
     /**
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
      */
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 
